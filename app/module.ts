c2f82b578d584974b53ae0f68e04685c2c6f938f
FreeMasen <RFM@Herman.local>
2016-11-06 11:11:19 -0600

    add angular material

--- app/module.ts
+++ app/module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { MaterialModule } from '@angular/material'
 
 import { AppComponent } from './component'
 import { Dashboard } from './dashboard/component'
 
 import { Router } from './router/module'
+import { Pages } from './services/pages'
+
+import './util/rxjs'
 
 @NgModule({
     imports: [
+        MaterialModule.forRoot(),
         BrowserModule,
         Router
     ],
     declarations: [
         AppComponent,
         Dashboard
+    ],
+    providers: [
+        Pages
     ],
     bootstrap: [
         AppComponent
