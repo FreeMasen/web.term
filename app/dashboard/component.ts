391f6a39a9e144868e5e48b6d9c52efda7e6fd60
FreeMasen <RFM@Herman.local>
2016-11-07 21:29:19 -0600

    update dashboard to include pages service

--- app/dashboard/component.ts
+++ app/dashboard/component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+
+
+import { Pages } from '../services/pages'
 
 @Component({
     selector: 'dashboard',
     templateUrl: './app/dashboard/template.html'
 })
-export class Dashboard {
+
+export class Dashboard implements OnInit {
+    name = 'RobertMasen.pizza'
+    pages = []
+    private pageObserver
+    private pageStream
+
+    constructor(private pageService: Pages) {}
+
+    ngOnInit() {
+        console.log('onInit')
+        this.pageObserver = this.pageService.get()
+        this.pageStream = this.pageObserver.subscribe(page => {
+            this.pages.push(page)
+        }, err => {
+            console.log(err)
+        })
+    }
 
 }
