db2b77126ac1e26aa6130d228fe6f4ae5b42a9bc
FreeMasen <RFM@Herman.local>
2016-11-12 15:32:27 -0600

    add new routes

--- app/router/module.ts
+++ app/router/module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
 import { Dashboard } from '../dashboard/component'
+import { Pages } from '../pages/component'
+import { PageDetail } from '../pageDetail/component'
+import { Messages } from '../messages/component'
+import { MessageDetail } from '../messageDetail/component'
+import { Metrics } from '../metrics/component'
+import { MetricDetail } from '../metricDetail/component'
 
 const routes: Routes = [
     {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-    {path: 'dashboard', component: Dashboard }
+    {path: 'dashboard', component: Dashboard },
+    {path: 'pages', component: Pages},
+    {path: 'pages/:id', component: PageDetail },
+    {path: 'messages', component: Messages},
+    {path: 'messages/:id', component: MessageDetail},
+    {path: 'analytics', component: Metrics},
+    {path: 'analytics/:id', component: MetricDetail},
 ]
 
 @NgModule({
