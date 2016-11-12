4f39e80eec1d385fa67d64507e517d7685f6c55c
FreeMasen <RFM@Herman.local>
2016-11-12 15:34:00 -0600

    update to include all services

--- app/dashboard/component.ts
+++ app/dashboard/component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
 
 
-import { Pages } from '../services/pages'
+import { PagesService } from '../services/pagesService'
+import { MessagesService } from '../services/messagesService'
+import { AnalyticsService } from '../services/analyticsService'
 
 @Component({
     selector: 'dashboard',
@@ -11,19 +14,43 @@ import { Pages } from '../services/pages'
 export class Dashboard implements OnInit {
     name = 'RobertMasen.pizza'
     pages = []
+    messages = []
+    metrics = []
     private pageObserver
     private pageStream
+    private messageObserver
+    private messageStream
+    private metricsObserver
+    private metricsStream
 
-    constructor(private pageService: Pages) {}
+    constructor(
+        private router: Router,
+        private pageService: PagesService,
+        private messageService: MessagesService,
+        private analyticsService: AnalyticsService
+        ) {}
 
     ngOnInit() {
-        console.log('onInit')
         this.pageObserver = this.pageService.get()
         this.pageStream = this.pageObserver.subscribe(page => {
             this.pages.push(page)
         }, err => {
-            console.log(err)
+            console.error('Pages error', err)
         })
+        this.messageObserver = this.messageService.getUnread()
+        this.messageStream = this.messageObserver.subscribe(message => {
+            this.messages.push(message)
+        }, err => {
+            console.error('Messages Error', err)
+        })
+        this.metricsObserver = this.analyticsService.getAnalyticsData()
+        this.metricsStream = this.metricsObserver.subscribe(metric => {
+            this.metrics.push(metric)
+        })
+    }
+
+    goToPage(page) {
+        this.router.navigate(['/pages', page.name])
     }
 
 }
